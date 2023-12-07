a888da75bd2c11078f7e6c309667e8b7f8bde9f6
AVINEX9\ankur <ankur@avinex.in>
2023-12-07 16:23:26 +0530

    skdnsk

--- Ankur_revamp_App_Servises/Revamp_Ank_App/Revamp_Ank_App/obj/Debug/net8.0/Revamp_Ank_App.AssemblyInfo.cs
+++ Ankur_revamp_App_Servises/Revamp_Ank_App/Revamp_Ank_App/obj/Debug/net8.0/Revamp_Ank_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revamp_Ank_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706f7236a859bf05e123ce13e33beae9d7ce86f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revamp_Ank_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revamp_Ank_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
